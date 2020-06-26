06d836ea320d68f5a2c7785525033056377ba5f3
Sahil Islam <thesahil.islam@gmail.com>
2020-06-26 18:13:28 +0530

    The seperate javascript file

---
+++ testjavascript.js
@@ -0,0 +1,23 @@
+
+document.write(" Javascript ");
+
+var time;	//Declaring a variable without assigning a value.
+var date=26062020;	//Assigning a value to a variable.
+
+var today="Today is "
+
+document.write(today);
+document.write(date);
+
+alert("Warning !!") // alert is a inbuilt function for Pops ups.
+
+document.write("Seperate js file");
+
+//Function
+
+function f(){   // This is a function with no argument.
+    
+    document.write("This is a function");    
+}
+
+f();
